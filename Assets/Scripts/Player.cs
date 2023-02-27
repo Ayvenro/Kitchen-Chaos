a8eaaaefe2dbfd2557275c1e01e9f28c77f537b7
FantomAda <fantomada@yandex.ru>
2023-02-27 13:43:38 +0300

    Implement new Unity Input System
    Movement code is refactored, added cinemachine camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private InputAction move;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float rotateSpeed = 9f;
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        inputVector = inputVector.normalized;
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        isWalking = moveDirection != Vector3.zero;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
