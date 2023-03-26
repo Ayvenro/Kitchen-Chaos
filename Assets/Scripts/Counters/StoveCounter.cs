edfeab40a4d12ade0ec9abbe8259decece927602
FantomAda <46055882+Ayvenro@users.noreply.github.com>
2023-03-26 14:00:12 +0300

    Remove unused namespaces

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static CuttingCounter;
-
 public class StoveCounter : BaseCounter, IHasProgress
 {
     public event EventHandler<OnStateEventArgs> OnStateChanged;
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class SoundManager : MonoBehaviour
 {
